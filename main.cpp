7ef644fc57a682c1baf9eb2018fac51bdca5ed58
eman emair <134128894+emanemair@users.noreply.github.com>
2024-01-09 12:27:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@ using namespace std;
 
 int main()
 {
-	cout << fNum(2, 3, 4) << "\n"; 
+	int firstFibNum;
+	int secondFibNum;
+	int nth;
+	cout << "Enter the first Fibonacci number: ";
+	cin >> firstFibNum;
+	cout << endl;
+	cout << "Enter the second Fibonacci number: ";
+	cin >> secondFibNum;
+	cout << endl;
+	cout << "Enter the position of the desired Fibonacci number: ";
+	cin >> nth;
+	cout << endl;
+	cout << "The Fibonacci number at position " << nth
+	<< " is: " << fNum(firstFibNum, secondFibNum, nth)
+	<< endl;
 	return 0; 
 }
